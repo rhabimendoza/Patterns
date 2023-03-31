55b9f05adf64b4e13751629d5a230bb083ff8790
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-03-31 18:30:31 +0800

    Update Pattern24.c

--- Pattern24.c
+++ Pattern24.c
@@ -12,10 +12,13 @@
 
 int main(){
 
+    // Create n
     int n;
 
+    // Get n
     scanf("%d", &n);
 
+    // Output pattern
     int num = 1;
     for(int ctr = 0; ctr < n; ctr++){
         for(int ctr2 = 0; ctr2 <= ctr; ctr2++){
