30c9c0590883d48cddefc6f2ce9e21c9fa844128
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-03-31 18:30:09 +0800

    Add files via upload

---
+++ Pattern25.c
@@ -0,0 +1,30 @@
+/*
+
+1 2 3 4 5
+6 7 8 9
+10 11 12
+13 14
+15
+
+*/
+
+#include<stdio.h>
+
+int main(){
+
+    // Create n
+    int n;
+
+    // Get n
+    scanf("%d", &n);
+
+    // Output pattern
+    int num = 1;
+    for(int ctr = n; ctr >= 1; ctr--){
+        for(int ctr2 = 0; ctr2 < ctr; ctr2++){
+            printf("%d ", num);
+            num++;
+        }
+        printf("\n");
+    }
+}
