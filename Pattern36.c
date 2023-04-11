9bb6d563987d00ea80bd91c6e58ff3c56167a1bb
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-04-11 18:33:27 +0800

    Add files via upload

---
+++ Pattern36.c
@@ -0,0 +1,30 @@
+/*
+
+A
+BC
+DEF
+GHIJ
+KLMNO
+
+*/
+
+#include<stdio.h>
+
+int main(){
+    
+    // Create n
+    int n;
+
+    // Get n
+    scanf("%d", &n);
+
+    // Output pattern
+    int num = 65;
+    for(int ctr = 0; ctr < n; ctr++){
+        for(int ctr2 = 0; ctr2 <= ctr; ctr2++){
+            printf("%c", num);
+            num++;
+        }
+        printf("\n");
+    }
+}
